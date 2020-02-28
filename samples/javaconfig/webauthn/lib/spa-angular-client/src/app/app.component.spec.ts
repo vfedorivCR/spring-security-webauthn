5ca41eff1d35213575744006b508a630047954a5
Vitalii Fedoriv <vitalii.fedoriv@chromeriver.com>
2020-02-28 18:41:55 +0200

    update html - 5

--- samples/javaconfig/webauthn/lib/spa-angular-client/src/app/app.component.spec.ts
+++ samples/javaconfig/webauthn/lib/spa-angular-client/src/app/app.component.spec.ts
@@ -52,10 +52,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Spring Security WebAuthn Sample SPA'`, async(() => {
+  it(`should have as title 'Fingerprint test app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Spring Security WebAuthn Sample SPA');
+    expect(app.title).toEqual('Fingerprint test app');
   }));
   // it('should render title in a h1 tag', async(() => {
   //   const fixture = TestBed.createComponent(AppComponent);
